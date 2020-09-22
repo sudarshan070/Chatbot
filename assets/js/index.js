168f1011eafa949147a3b113b1a86ca83c47ee3d
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-09-22 23:15:29 +0530

    bakcground color add

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+function openBot() {
+    document.getElementById("chatBot").style.display = "block";
+    document.querySelector(".popup-open-btn").style.display = "none";
+}
+function closeBot() {
+    document.getElementById("chatBot").style.display = "none";
+    document.querySelector(".popup-open-btn").style.display = "block";
+}
