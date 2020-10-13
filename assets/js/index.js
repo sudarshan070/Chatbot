db1c264b953425e9b97f06ee2d6e164c5fcbf5ee
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-10-13 21:56:55 +0530

    button animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,15 @@
 function openBot() {
-    document.getElementById("chatBot").style.display = "block";
-    document.querySelector(".popup-open-btn").style.display = "none";
+    var openBot = document.getElementById("chatBot")
+    openBot.classList.add('openBot')
+    openBot.classList.remove('closeBot')
+    openBot.classList.remove("outer-hide")
+    document.querySelector(".popup-open-btn").style.display = "none"
 }
 function closeBot() {
-    document.getElementById("chatBot").style.display = "none";
+    var closeBot = document.getElementById("chatBot")
+    closeBot.classList.remove('openBot')
+    closeBot.classList.add('closeBot')
+
+
     document.querySelector(".popup-open-btn").style.display = "block";
 }
